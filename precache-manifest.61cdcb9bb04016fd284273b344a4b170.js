32488252e0d792b1319ed6299e6bfcf075a8cf05
Mikkel Jakobsen <mikkel@adaptagency.com>
2020-03-31 15:50:20 +0200

    Adding new build

---
+++ precache-manifest.61cdcb9bb04016fd284273b344a4b170.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c613da26d00d472065fc8fb4d48ef406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26122878c18d4b61665f",
+    "url": "/static/css/main.2cea3962.chunk.css"
+  },
+  {
+    "revision": "909c1cea7978f7db8997",
+    "url": "/static/js/2.110d369b.chunk.js"
+  },
+  {
+    "revision": "71db9edcb13bf1be145ab49761bd2bc2",
+    "url": "/static/js/2.110d369b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26122878c18d4b61665f",
+    "url": "/static/js/main.e01161f4.chunk.js"
+  },
+  {
+    "revision": "7ea0d5e52f7ae425c059",
+    "url": "/static/js/runtime-main.46865f95.js"
+  },
+  {
+    "revision": "2ecb426f85ffc1c53b677556210e629f",
+    "url": "/static/media/fira-sans-v10-latin-700.2ecb426f.woff2"
+  },
+  {
+    "revision": "4d2dfa0d7366eb8ba6e2d9cc5ef6e58d",
+    "url": "/static/media/fira-sans-v10-latin-700.4d2dfa0d.woff"
+  },
+  {
+    "revision": "78773521b0ffe376bc7edd8ec2a591fb",
+    "url": "/static/media/fira-sans-v10-latin-regular.78773521.woff2"
+  },
+  {
+    "revision": "baa3b51e20ff84bf4964223288c0ba10",
+    "url": "/static/media/fira-sans-v10-latin-regular.baa3b51e.woff"
+  },
+  {
+    "revision": "fc1b07604513fbf51d8b5f724fa873c7",
+    "url": "/static/media/icon-check.fc1b0760.svg"
+  },
+  {
+    "revision": "94feb6d2b04879b50734358d4595908e",
+    "url": "/static/media/result-icon-assault.94feb6d2.svg"
+  },
+  {
+    "revision": "37af0599122181e8eacdeea1f06508cd",
+    "url": "/static/media/result-icon-breakin.37af0599.svg"
+  },
+  {
+    "revision": "98c5bc7064f016d400a95731d0dd943a",
+    "url": "/static/media/result-icon-fire.98c5bc70.svg"
+  },
+  {
+    "revision": "0ae190ad44dbc138d51382405fb72614",
+    "url": "/static/media/verisure-home.0ae190ad.png"
+  },
+  {
+    "revision": "39cf59c6956e5345ccb119bdd9d8d90c",
+    "url": "/static/media/verisure-portal.39cf59c6.png"
+  }
+]);
